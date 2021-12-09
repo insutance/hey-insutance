3c897473767f9897e3ee55147fb4aa39d21bc79e
insutance <insutance@naver.com>
2021-12-10 00:25:17 +0900

    feat: question name, age

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="insutance",
+    version="0.0.1",
+    description="Hey insutance! Who are you?",
+    author="insutance",
+    author_email="insutance@naver.com",
+    url="https://github.com/insutance/hey-insutance",
+    python_requires=">= 3.8",
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "hey = insutance.main:main"
+        ]
+    },
+    package_data={},
+    include_package_data=True
+)
